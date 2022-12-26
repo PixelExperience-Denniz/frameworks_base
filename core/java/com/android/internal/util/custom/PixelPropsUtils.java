ccc9b21ebff47c842ee7debeb31376c6d2323513
SakilMondal <mondalsakil@gmail.com>
2022-12-26 19:01:03 +0000

    PixelPropsUtils: Update for december security patch
    Change-Id: Ib6d1c91275cde25bccfd681035bdf6caa322c21f

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -141,7 +141,7 @@ public class PixelPropsUtils {
         propsToChangePixel6.put("DEVICE", "raven");
         propsToChangePixel6.put("PRODUCT", "raven");
         propsToChangePixel6.put("MODEL", "Pixel 6 Pro");
-        propsToChangePixel6.put("FINGERPRINT", "google/raven/raven:13/TP1A.221105.002/9080065:user/release-keys");
+        propsToChangePixel6.put("FINGERPRINT", "google/raven/raven:13/TQ1A.221205.011/9244662:user/release-keys");
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
